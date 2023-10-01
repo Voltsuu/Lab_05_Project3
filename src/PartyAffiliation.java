0e32dd278da1f3307fbae2d0ff0057f9bb9cd3ea
Voltsuuu <npoudel2007@gmail.com>
2023-10-01 19:13:47 -0400

    Lab_05_Project3

---
+++ src/PartyAffiliation.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String affiliation = "";
+
+        System.out.println("What is your political affiliation? (Respond with D,R, or I)");
+        affiliation = scan.nextLine();
+        if (affiliation.equalsIgnoreCase("D")) {
+            System.out.println("You get a Democratic Donkey.");
+        } else if (affiliation.equalsIgnoreCase("R")) {
+            System.out.println("You get a Republican Elephant.");
+        } else if (affiliation.equalsIgnoreCase("I"))
+            System.out.println("You get an Independent Man");
+        else
+            System.out.println("You have entered an invalid affiliation");
+    }
+}
